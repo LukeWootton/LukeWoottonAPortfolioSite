60a2a83ddcc7090a8c7d6156f3b41ea67b6d0c74
LukeWootton <lukejwootton@gmail.com>
2020-12-12 01:46:33 +0000

    Added Scroll_Top_Button Added JavaScript file Need to fix Scroll_Top_Button

---
+++ JS_Website_Assignment.js
@@ -0,0 +1,6 @@
+var Scroll_Top_Button = document.getElementById("Scroll_Top_Button");
+
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
