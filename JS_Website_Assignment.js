6ed1c09716d87277f21d1bcfc649d5c411b0a557
LukeWootton <lukejwootton@gmail.com>
2020-12-12 16:02:54 +0000

    Fixed Scroll_Top_Button

--- JS_Website_Assignment.js
+++
@@ -1,6 +0,0 @@
-var Scroll_Top_Button = document.getElementById("Scroll_Top_Button");
-
-function topFunction() {
-    document.body.scrollTop = 0; // For Safari
-    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-}
